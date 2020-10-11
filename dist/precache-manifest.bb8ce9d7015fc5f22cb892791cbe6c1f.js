e81ab5bf4327c724fdb8304e81591a84f3b39d15
Carlos Retamal <carlosretamal@MacBook-Pro-de-Carlos.local>
2020-10-11 19:24:26 -0300

    agregado dist

---
+++ dist/precache-manifest.bb8ce9d7015fc5f22cb892791cbe6c1f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e0c5d330feb2088b654",
+    "url": "/css/chunk-vendors.2e1463f9.css"
+  },
+  {
+    "revision": "6f96be79b1381c433f7931c655fca84d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c491df8cc82fd1e8eef",
+    "url": "/js/about.ee4e85f9.js"
+  },
+  {
+    "revision": "b8a11cb8ca7fbaf02fcc",
+    "url": "/js/app.3f542945.js"
+  },
+  {
+    "revision": "2e0c5d330feb2088b654",
+    "url": "/js/chunk-vendors.c6e974d2.js"
+  },
+  {
+    "revision": "5392f0644a908370c071217cc25f6dbf",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
